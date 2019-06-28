dad37dd19a7f20ed6e0e1edb8f1f7227cd421e93
wangby <wangby92@chinaunicom.cn>
2019-06-28 16:59:13 +0800

    20190628-练习项目初始化

--- src/main/java/buy/practice/lamda/controller/PracticeController.java
+++ src/main/java/buy/practice/lamda/controller/PracticeController.java
@@ -1,4 +1,4 @@
-package buy.practice.lamda;
+package buy.practice.lamda.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
